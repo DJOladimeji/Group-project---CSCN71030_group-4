6d31f5e9470c2e91a715b4f3244d6797cf5d49be
Ilyas Yusuf <Iyusuf3055@conestogac.on.ca>
2022-03-29 19:03:03 -0400

    Changed all the functions to reflect the changes in the design. Changed the function definitions.

--- Priority HMS/HospitalModule.c
+++ Priority HMS/HospitalModule.c
@@ -5,206 +5,78 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
-void allocatememory(HOSPITAL hospital[10][2]) {
-	for (int i = 0; i < 10; i++) {
-
-		hospital[i][0].Name = (char*)malloc(MAXSTRINGLENGTH);
-		hospital[i][1].Name = (char*)malloc(MAXSTRINGLENGTH);
-		if (hospital[i][0].Name && hospital[i][1].Name) {
-			memset(hospital[i][0].Name, 0, (MAXSTRINGLENGTH));
-			memset(hospital[i][1].Name, 0, (MAXSTRINGLENGTH));
-		}
-		else
-		{
-			fprintf(stderr, "error allocating memory!\n");
-			exit(1);
-		}
-	}
-}
-
-FILE* Read_Hospital_File(FILE* fp, HOSPITAL hospital[10][2], unsigned int num) {
-
-
-	switch (num)
-	{
-	case(1):
-
-		if ((fp = fopen("StMary.txt", "r")) == NULL) {
-			fprintf(stderr, "Error reading file\n\n");
-		}
-		for (int i = 0; i < 10; i++) {
-			if (fp) {
-				if (fscanf(fp, "%s %d %d", hospital[i][0].Name, &hospital[i][0].patientnum, &hospital[i][0].unique) == 3)
-				{
-				}
-			}
-		}
-		break;
-	case(2):
-		if ((fp = fopen("Cambridge.txt", "r")) == NULL) {
-			fprintf(stderr, "Error reading file\n\n");
-		}
-		if (fp) {
-			for (int i = 0; i < 10; i++) {
-				if (fp) {
-					if (fscanf(fp, "%s %d %d", hospital[i][1].Name, &hospital[i][1].patientnum, &hospital[i][1].unique) == 3)
-					{
-					}
-				}
-			}
-		}
-		break;
-	default:
-		break;
-	}
 
 
 
-	return fp;
-}
-void Save_Hospital_File(FILE* fp, HOSPITAL hospital[10][2], unsigned int num) {
-	switch (num)
-	{
-	case(1):
-		if ((fp = fopen("StMary.txt", "w")) == NULL) {
-			fprintf(stderr, "Error reading file\n\n");
-		}
-		fclose(fp);
-		break;
-	case(2):
-		if ((fp = fopen("Cambridge.txt", "w")) == NULL) {
-			fprintf(stderr, "Error reading file\n\n");
-		}
-		if (fp) {
-			for (int i = 0; i < 10; i++) {
-				fprintf(fp, "%s %d %d", hospital[i][1].Name, hospital[i][1].patientnum, hospital[i][1].unique);
-			}
-		}
-		fclose(fp);
-		break;
-	case(3):
-		if ((fp = fopen("GrandRiver.txt", "w")) == NULL) {
-			fprintf(stderr, "Error reading file\n\n");
-		}
-		fclose(fp);
-		break;
-	default:
-		break;
-	}
-}
-
-void Add_to_Hospital(HOSPITAL newhospital[10][2], char* letter, char* hospitalname) {
-	bool Ishere;
-	Ishere = Check_Hospital(newhospital, letter);
+bool Add_to_Hospital(char* docfilename, char* hospital[10], unsigned int hospitalchoice) {
+	bool Ishere = true;
+	bool IsAdded = false;
+	unsigned int index = 0;
+	Ishere = Check_Hospital(docfilename, hospital, &index);
 	if (!Ishere) {
+		//put name into array
 		for (int i = 0; i < 10; i++) {
-			if (strcmp(hospitalname, "StMary.txt") == 0) {
-				if (strcmp(newhospital[i][0].Name, "") == 0) {
-					newhospital[i][0].Name = letter;
-					newhospital[i][0].patientnum = 4;
-					newhospital[i][0].unique = 2;
-					break;
-				}
-			}
-			else if (strcmp(newhospital[i][1].Name, "GrandRiver.txt") == 0) {
-				if (strcmp(newhospital[i][1].Name, "") == 0) {
-					newhospital[i][1].Name = letter;
-					newhospital[i][1].patientnum = 6;
-					newhospital[i][1].unique = 3;
-					break;
-				}
-			}
-		}
-	}
-	else
-	{
-		printf("file already in system!\n");
-	}
-}
-void Delete_from_Hospital(HOSPITAL newhospital[10][2], char* letter, char* hospitalname) {
-	bool Ishere;
-	Ishere = Check_Hospital(newhospital, letter);
-	for (int i = 0; i < 10; i++) {
-		if (Ishere) {
-			if (newhospital[i][0].Name == letter)
-			{
-				newhospital[i][0].Name = '\0';
-				newhospital[i][0].patientnum = 0;
-				newhospital[i][0].unique = 0;
-				break;
-			}
-			else if (newhospital[i][1].Name == letter)
-			{
-				newhospital[i][1].Name = '\0';
-				newhospital[i][1].patientnum = 0;
-				newhospital[i][1].unique = 0;
+			if (strcmp(hospital[i], "") == 0) {
+				strcpy(hospital[i], docfilename);
+				IsAdded = true;
 				break;
 			}
 		}
 	}
+	return IsAdded;
 }
-bool Check_Hospital(HOSPITAL newhospital[10][2], char* letter) {
+bool Delete_from_Hospital(char* docfilename, char* hospital[10], unsigned int hospitalchoice) {
+	bool Ishere = false;
+	bool IsDeleted = false;
+	unsigned int index;
+	Ishere = Check_Hospital(docfilename, hospital, &index);
+	if (Ishere) {
+		strcpy(hospital[index], "");
+		IsDeleted = true;
+	}
+	return IsDeleted;
+}
+bool Check_Hospital(char* docfilename, char* hospital[10], unsigned int* index) {
 	bool Ishere = false;
 	for (int i = 0; i < 10; i++) {
-		if (newhospital[i][0].Name) {
-			if (strcmp(newhospital[i][0].Name, letter) == 0)
-			{
-				Ishere = true;
-				break;
-			}
-			else if (strcmp(newhospital[i][1].Name, letter) == 0)
-			{
-				Ishere = true;
-				break;
-			}
+		if (strcmp(docfilename, hospital[i]) == 0) {
+			Ishere = true;
+			*index = i;
 		}
-	}	return Ishere;
+	}
+	return Ishere;
 }
-void Display_Population(HOSPITAL currenthospital[10][2], char* hospitalname) {
-	char hosp1[20] = { 'I', 'L', 'Y', 'A', 'S','\0' };
+char* getdocfilefromhospital(char* username, char* hospital[10]) {
+
+	char* filename;
+	for (int i = 0; i < 10; i++) {
+		filename = strstr(hospital[i], username);
+		if (filename != 0) {
+			break;
+		}
+	}
+	return filename;
+}
+int Display_Population(char* hospital[10]) {
+
 	unsigned int counter = 0;
 	for (int i = 0; i < 10; i++) {
-		if (strcmp(hospitalname, hosp1) == 0) {
-			if (strcmp(currenthospital[i][0].Name, "") != 0) {
+		if (*hospital[i] != 0) {
+			if (strcmp(hospital[i], "") != 0) {
 				counter++;
 			}
 		}
-		else {
-			if (strcmp(currenthospital[i][1].Name, "") != 0) {
-				counter++;
-			}
 
-		}
 	}
-	printf("%d doctors in hospital", counter);
+	return counter;
 }
-
-
-char* createstring()
-{
-	char* allocatedstring;
-	char input[MAXSTRINGLENGTH];
-	size_t len = 1;
-	//Critical error detected c0000374 sometimes raised.
-	if (fgets(input, MAXSTRINGLENGTH, stdin) != NULL) {
-		input[strlen(input) - 1] = NULL;
-		len = strlen(input);
-		//Critical error detected c0000374 sometimes raised.
-		allocatedstring = (char*)malloc(len + 2);
-		//checks of allocation was successful
-		if (allocatedstring)
-		{
-			memset(allocatedstring, 0, ((len + 1)));
-			strcpy(allocatedstring, input);
+void printhospital(char** hospital) {
+	for (int i = 0; i < 10; i++) {
+		if (*hospital[i] != 0) {
+			if (strcmp(hospital[i], "") != 0)
+				printf("%s\n", hospital[i]);
 		}
-		else
-		{
-			fprintf(stderr, "error allocating memory!\n");
-			exit(1);
-		}
-		return allocatedstring;
 	}
-
 
 }
 
--- Priority HMS/HospitalModule.h
+++ Priority HMS/HospitalModule.h
@@ -1,19 +1,12 @@
 #pragma once
 #include <stdio.h>
 #include <stdbool.h>
-typedef struct hospital {
 
-	char* Name;
-	unsigned int patientnum;
-	unsigned int unique;
 
-}HOSPITAL;
+bool Add_to_Hospital(char*, char* [10], unsigned int);
+bool Delete_from_Hospital(char*, char* [10], unsigned int);
+bool Check_Hospital(char*, char* [10], unsigned int*);
+int Display_Population(char* [10]);
+char* getdocfilefromhospital(char*, char* [10]);
+void printhospital(char**);
 
-FILE* Read_Hospital_File(FILE*, HOSPITAL[10][2], unsigned int);
-void Save_Hospital_File(FILE*, HOSPITAL[10][2], unsigned int);
-void allocatememory(HOSPITAL[10][2]);
-void Add_to_Hospital(HOSPITAL[10][2], char*, char*);
-void Delete_from_Hospital(HOSPITAL[10][2], char*, char*);
-bool Check_Hospital(HOSPITAL[10][2], char*);
-void Display_Population(HOSPITAL[10][2], char*);
-char* createstring();
